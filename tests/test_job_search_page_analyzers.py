9d2dc26df00e8cbdd30df39f048133669e36be3b
Takuma Kira <takuma@kirakiraworx.com>
2024-01-20 13:24:07 +0900

    [refactor]: Add tests for GlassdoorJobSearchPageAnalyzer

--- tests/test_job_search_page_analyzers.py
+++ tests/test_job_search_page_analyzers.py
@@ -4,25 +4,26 @@ from src.app.job_search_page_analyzers import create_analyzer
 from src.app.job_search_page_analyzers.glassdoor import GlassdoorJobSearchPageAnalyzer
 
 
-def test_create_analyzer_returns_glassdoor_analyzer_when_target_url_is_glassdoor():
-    target_url = 'https://www.glassdoor.com/Job/ocation-title-jobs-UNKNOWN_STRINGS.htm'
-    target_job_title = 'title'
-    target_job_location = 'location'
+class TestCreateAnalyzer:
+    def test_create_analyzer_returns_glassdoor_analyzer_when_target_url_is_glassdoor(self):
+        target_url = 'https://www.glassdoor.com/Job/ocation-title-jobs-UNKNOWN_STRINGS.htm'
+        target_job_title = 'title'
+        target_job_location = 'location'
 
-    with patch.object(GlassdoorJobSearchPageAnalyzer, '__init__', return_value=None) as mock_init:
-        analyzer = create_analyzer(target_url, target_job_title, target_job_location)
+        with patch.object(GlassdoorJobSearchPageAnalyzer, '__init__', return_value=None) as mock_init:
+            analyzer = create_analyzer(target_url, target_job_title, target_job_location)
 
-    assert isinstance(analyzer, GlassdoorJobSearchPageAnalyzer)
-    mock_init.assert_called_once_with(target_job_title, target_job_location)
+        assert isinstance(analyzer, GlassdoorJobSearchPageAnalyzer)
+        mock_init.assert_called_once_with(target_job_title, target_job_location)
 
-def test_create_analyzer_throws_when_target_url_is_unknown():
-    target_url = 'https://www.unknown.com/Job/ocation-title-jobs-UNKNOWN_STRINGS.htm'
-    target_job_title = 'title'
-    target_job_location = 'location'
+    def test_create_analyzer_throws_when_target_url_is_unknown(self):
+        target_url = 'https://www.unknown.com/Job/ocation-title-jobs-UNKNOWN_STRINGS.htm'
+        target_job_title = 'title'
+        target_job_location = 'location'
 
-    try:
-        create_analyzer(target_url, target_job_title, target_job_location)
-    except Exception as e:
-        assert str(e) == f"Unsupported target URL: {target_url}"
-    else:
-        assert False
+        try:
+            create_analyzer(target_url, target_job_title, target_job_location)
+        except Exception as e:
+            assert target_url in str(e)
+        else:
+            assert False
